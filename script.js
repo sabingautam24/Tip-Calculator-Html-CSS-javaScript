f1a3ca4ae12537b4005250cd9c58e574382e4849
Sabin Gautam <123347155+SabinGautam24@users.noreply.github.com>
2023-02-11 11:38:37 +0545

    Tip Calculator - Sabin Gautam

---
+++ script.js
@@ -0,0 +1,43 @@
+// custom function
+
+function calculateTip() {
+  //store the data of input
+  var billAmount = document.getElementById("billAmount").value;
+  var serviceQuality = document.getElementById("serviceQuality").value;
+  var numPeople = document.getElementById("totalPeople").value;
+
+  //   quick validation
+  if (billAmount === "" || serviceQuality == 0) {
+    window.alert("Please enter some values to get this baby up and running!");
+    // return;// this will prevent the function form continuing
+  }
+
+  //   check ti see if this input us empty or less than or equal to 1
+  if (numPeople === "" || numPeople <= 1) {
+    numPeople = 1;
+
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //   do some Math!
+  var total = (billAmount * serviceQuality) / numPeople;
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
+
+  //Display the tip!
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+}
+
+// Hide the tip amount on load
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// clicking the button calls our custom function
+
+document.getElementById("calculate").onclick = function () {
+  calculateTip();
+};
